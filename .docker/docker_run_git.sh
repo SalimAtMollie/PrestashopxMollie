31bed5ecbb92a19fb01035ddcc19bf64aa9f6777
Salim Bouziane Rabhi <salim.bouziane.rabhi@mollie.com>
2022-09-14 15:04:43 +0200

    update

--- .docker/docker_run_git.sh
+++ .docker/docker_run_git.sh
@@ -1,56 +1,61 @@
 #!/bin/sh
 
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+NC=$(tput sgr0)
+
 if [ "${DISABLE_MAKE}" != "1" ]; then
-  echo "\n* Running composer ...";
+  echo "\n* ${YELLOW}[!] Running composer ...${NC}";
   runuser -g www-data -u www-data -- /usr/local/bin/composer install --no-interaction
 
-  echo "\n* Build assets ...";
+  echo "\n* ${YELLOW}[!] Build assets ...${NC}";
   runuser -g www-data -u www-data -- /usr/bin/make assets
 fi
 
 if [ "$DB_SERVER" = "<to be defined>" -a $PS_INSTALL_AUTO = 1 ]; then
-    echo >&2 'error: You requested automatic PrestaShop installation but MySQL server address is not provided '
-    echo >&2 '  You need to specify DB_SERVER in order to proceed'
+    echo >&2 "${RED}[!] Error: You requested automatic PrestaShop installation but MySQL server address is not provided ${NC}"
+    echo >&2 "${RED}           You need to specify DB_SERVER in order to proceed${NC}"
     exit 1
 elif [ "$DB_SERVER" != "<to be defined>" -a $PS_INSTALL_AUTO = 1 ]; then
     RET=1
     while [ $RET -ne 0 ]; do
-        echo "\n* Checking if $DB_SERVER is available..."
+        echo "\n* ${YELLOW}[!] Checking if $DB_SERVER is available...${NC}"
         mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD -e "status" > /dev/null 2>&1
         RET=$?
 
         if [ $RET -ne 0 ]; then
-            echo "\n* Waiting for confirmation of MySQL service startup";
+            echo "\n* ${YELLOW}[!] Waiting for confirmation of MySQL service startup${NC}";
             sleep 5
         fi
     done
-        echo "\n* DB server $DB_SERVER is available, let's continue !"
+        echo "\n* ${GREEN}[!] DB server $DB_SERVER is available, let's continue !${NC}"
 fi
 
 # From now, stop at error
 set -e
 
 if [ $PS_DEV_MODE -ne 1 ]; then
-  echo "\n* Disabling DEV mode ...";
+  echo "\n* ${YELLOW}[!] Disabling DEV mode ...${NC}";
   sed -ie "s/define('_PS_MODE_DEV_', true);/define('_PS_MODE_DEV_',\ false);/g" /var/www/html/config/defines.inc.php
 fi
 
 if [ ! -f ./config/settings.inc.php ]; then
     if [ $PS_INSTALL_AUTO = 1 ]; then
 
-        echo "\n* Installing PrestaShop, this may take a while ...";
+        echo "\n* ${YELLOW}[!] Installing PrestaShop, this may take a while ...${NC}";
 
         if [ $PS_ERASE_DB = 1 ]; then
-            echo "\n* Drop & recreate mysql database...";
+            echo "\n* ${YELLOW}[!] Drop & recreate mysql database...${NC}";
             if [ $DB_PASSWD = "" ]; then
-                echo "\n* Dropping existing database $DB_NAME..."
+                echo "\n* ${YELLOW}[!] Dropping existing database $DB_NAME...${NC}"
                 mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -e "drop database if exists $DB_NAME;"
-                echo "\n* Creating database $DB_NAME..."
+                echo "\n* ${YELLOW}[!] Creating database $DB_NAME...${NC}"
                 mysqladmin -h $DB_SERVER -P $DB_PORT -u $DB_USER create $DB_NAME --force;
             else
-                echo "\n* Dropping existing database $DB_NAME..."
+                echo "\n* ${YELLOW}[!] Dropping existing database $DB_NAME...${NC}"
                 mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD -e "drop database if exists $DB_NAME;"
-                echo "\n* Creating database $DB_NAME..."
+                echo "\n* ${YELLOW}[!] Creating database $DB_NAME...${NC}"
                 mysqladmin -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD create $DB_NAME --force;
             fi
         fi
@@ -59,7 +64,7 @@ if [ ! -f ./config/settings.inc.php ]; then
             export PS_DOMAIN=$(hostname -i)
         fi
 
-        echo "\n* Launching the installer script..."
+        echo "\n* ${YELLOW}[!] Launching the installer script...${NC}"
         runuser -g www-data -u www-data -- php /var/www/html/$PS_FOLDER_INSTALL/index_cli.php \
         --domain="$PS_DOMAIN" --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
         --db_password=$DB_PASSWD --prefix="$DB_PREFIX" --firstname="Marc" --lastname="Beier" \
@@ -67,18 +72,25 @@ if [ ! -f ./config/settings.inc.php ]; then
         --all_languages=$PS_ALL_LANGUAGES --newsletter=0 --send_email=0 --ssl=$PS_ENABLE_SSL
 
         if [ $? -ne 0 ]; then
-            echo 'warning: PrestaShop installation failed.'
+            echo "${RED}[!] Error: PrestaShop installation failed.${NC}"
         fi
     fi
 else
-    echo "\n* PrestaShop Core already installed...";
+    echo "\n* ${GREEN}[!] PrestaShop Core already installed...${NC}";
 fi
 
 if [ $PS_DEMO_MODE -ne 0 ]; then
-    echo "\n* Enabling DEMO mode ...";
+    echo "\n* ${YELLOW}[!] Enabling DEMO mode ...${NC}";
     sed -ie "s/define('_PS_MODE_DEMO_', false);/define('_PS_MODE_DEMO_',\ true);/g" /var/www/html/config/defines.inc.php
 fi
 
-echo "\n* Almost ! Starting web server now\n";
+
+echo "${GREEN}############################################################################"
+echo "${GREEN}##### Installation Complete! Please activate Mollie in Extensions Page #####"
+echo "${GREEN}############################################################################"
+echo "${YELLOW}[!] Website Url 		 :${NC} http://localhost:8001"
+echo "${YELLOW}[!] Website Admin Url :${NC} http://localhost:8001/admin-dev"
+echo "${YELLOW}[!] Admin Username    :${NC} demo@prestashop.com"
+echo "${YELLOW}[!] Admin Password    :${NC} mollie"
 
 exec apache2-foreground
