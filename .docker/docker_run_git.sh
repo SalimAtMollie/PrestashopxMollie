5c965e98f780c47ab03b2ce68508989f5498e983
Salim Bouziane Rabhi <salim.bouziane.rabhi@mollie.com>
2022-09-30 13:44:18 +0200

    Optimization
    Removed elements that prestashop does not depend on to make the installation process quicker.

--- .docker/docker_run_git.sh
+++ .docker/docker_run_git.sh
@@ -80,7 +80,7 @@ if [ ! -f ./config/settings.inc.php ]; then
             echo "[!] Error: PrestaShop installation failed."
         else
             echo "\n* [!] Downloading Mollie plugin..."
-            wget -q --no-check-certificate --content-disposition "https://github.com/mollie/PrestaShop1.7/releases/download/V${MOLLIE_VERSION}/mollie.zip"           
+            wget -q --no-check-certificate --content-disposition "https://github.com/mollie/PrestaShop1.7/releases/download/${MOLLIE_VERSION}/mollie.zip"           
             unzip  -q ./mollie.zip
             mv ./mollie ./modules
         fi
